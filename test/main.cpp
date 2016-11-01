62e7238cb2273a1b72fa25305ee4af988059529f
MForever78 <shunjian1128@gmail.com>
2016-11-01 09:45:01 +0800

    fix: hash function marks as const

--- test/main.cpp
+++ test/main.cpp
@@ -8,99 +8,99 @@ using namespace std;
 
 TEST_CASE("can handle basic type with single thread", "[basic]") {
 
-  SECTION("int to int") {
-    Cache<int, int> cache(1024);
+    SECTION("int to int") {
+        Cache<int, int> cache(1024);
 
-    cache.set(0, 1024);
-    cache.set(1, 1024);
-    REQUIRE(cache.get(0) == 1024);
+        cache.set(0, 1024);
+        cache.set(1, 1024);
+        REQUIRE(cache.get(0) == 1024);
 
-    cache.set(0, 2048);
-    REQUIRE(cache.get(0) == 2048);
-  }
+        cache.set(0, 2048);
+        REQUIRE(cache.get(0) == 2048);
+    }
 
-  SECTION("string to int") {
-    Cache<string, int> cache(1024);
-    cache.set("foo", 1024);
-    cache.set("bar", 1024);
-    REQUIRE(cache.get("foo") == 1024);
+    SECTION("string to int") {
+        Cache<string, int> cache(1024);
+        cache.set("foo", 1024);
+        cache.set("bar", 1024);
+        REQUIRE(cache.get("foo") == 1024);
 
-    cache.set("foo", 2048);
-    REQUIRE(cache.get("foo") == 2048);
-  }
+        cache.set("foo", 2048);
+        REQUIRE(cache.get("foo") == 2048);
+    }
 
-  SECTION("string to string") {
-    Cache<string, string> cache(1024);
-    cache.set("foo", "lorem");
-    cache.set("bar", "ipsum");
-    REQUIRE(cache.get("foo") == "lorem");
+    SECTION("string to string") {
+        Cache<string, string> cache(1024);
+        cache.set("foo", "lorem");
+        cache.set("bar", "ipsum");
+        REQUIRE(cache.get("foo") == "lorem");
 
-    cache.set("foo", "long");
-    REQUIRE(cache.get("foo") == "long");
-  }
+        cache.set("foo", "long");
+        REQUIRE(cache.get("foo") == "long");
+    }
 }
 
 class Custom {
-public:
-  Custom(string _key, int _id) : key(_key), id(_id){};
-  string key;
-  int id;
+    public:
+        Custom(string _key, int _id) : key(_key), id(_id){};
+        string key;
+        int id;
 
-  bool operator==(const Custom &other) const {
-    return (key == other.key && id == other.id);
-  }
+        bool operator==(const Custom &other) const {
+            return (key == other.key && id == other.id);
+        }
 };
 
 template <> struct hash<Custom> {
-  size_t operator()(const Custom &c) {
-    return hash<string>()(c.key) ^ hash<int>()(c.id);
-  }
+    size_t operator()(const Custom &c) const {
+        return hash<string>()(c.key) ^ hash<int>()(c.id);
+    }
 };
 
 TEST_CASE("can handle custom type with single thread", "[custom]") {
-  SECTION("automatically") { Cache<Custom, int> cache(1024); }
+    SECTION("automatically") { Cache<Custom, int> cache(1024); }
 
-  SECTION("manually") {
-    struct CustomHasher {
-      size_t operator()(const Custom &c) const {
-        return hash<string>()(c.key) ^ hash<int>()(c.id);
-      }
-    };
+    SECTION("manually") {
+        struct CustomHasher {
+            size_t operator()(const Custom &c) const {
+                return hash<string>()(c.key) ^ hash<int>()(c.id);
+            }
+        };
 
-    Cache<Custom, int, CustomHasher> cache(1024);
-    Custom a("foo", 0);
-    Custom b("bar", 1);
+        Cache<Custom, int, CustomHasher> cache(1024);
+        Custom a("foo", 0);
+        Custom b("bar", 1);
 
-    cache.set(a, 1024);
-    cache.set(b, 2048);
-    REQUIRE(cache.get(a) == 1024);
+        cache.set(a, 1024);
+        cache.set(b, 2048);
+        REQUIRE(cache.get(a) == 1024);
 
-    cache.set(a, 2048);
-    REQUIRE(cache.get(a) == 2048);
-  }
+        cache.set(a, 2048);
+        REQUIRE(cache.get(a) == 2048);
+    }
 }
 
 mutex m;
 
 void runner(Cache<int, int> &cache, int l, int r) {
-  for (int i = l; i < r; ++i) {
-    cache.set(i, i);
-  }
+    for (int i = l; i < r; ++i) {
+        cache.set(i, i);
+    }
 
-  for (int i = l; i < r; ++i) {
-    int val = cache.get(i);
+    for (int i = l; i < r; ++i) {
+        int val = cache.get(i);
 
-    // use mutex to ensure REQUIRE's thread safety
-    lock_guard<mutex> guard(m);
-    REQUIRE(val == i);
-  }
+        // use mutex to ensure REQUIRE's thread safety
+        lock_guard<mutex> guard(m);
+        REQUIRE(val == i);
+    }
 }
 
 TEST_CASE("can handle multithreading", "[multithreading]") {
-  Cache<int, int> cache(2048);
+    Cache<int, int> cache(2048);
 
-  thread t1(runner, ref(cache), 0, 2000);
-  thread t2(runner, ref(cache), 1000, 2000);
-  t1.join();
-  t2.join();
+    thread t1(runner, ref(cache), 0, 2000);
+    thread t2(runner, ref(cache), 1000, 2000);
+    t1.join();
+    t2.join();
 }
